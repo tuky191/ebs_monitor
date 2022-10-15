288d910f73224f0832fe09fee1306c1413200279
tuky191 <michal@terra.money>
2022-10-15 21:33:16 +0200

    adding region

--- monitor.py
+++ monitor.py
@@ -17,7 +17,7 @@ load_dotenv()
 
 
 def get_volumes():
-    client = boto3.client('ec2')
+    client = boto3.client('ec2', region_name='us-east-1')
     response = client.describe_volumes(
         Filters=[
             {
